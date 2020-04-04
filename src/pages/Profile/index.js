3a15bdf54d01281099ec4681a8c20433786de58b
leosdesousa12 <leosdesousa@gmail.com>
2020-04-04 02:18:24 -0300

    ad function logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi'; 
 
 import api from '../../services/api';
@@ -9,6 +9,8 @@ import './style.css';
 import logoImg from '../../assets/logo.svg';
 export default function Profile(){
   const [incidents, setIncidents ] = useState([]);
+
+  const history = useHistory();
 
   const nameOng = localStorage.getItem('ongName');
   const ongId = localStorage.getItem('ongId');
@@ -38,6 +40,10 @@ export default function Profile(){
     }
   }
 
+  function handleLougout(){
+    localStorage.clear();
+    history.push('/');
+  }
   return (
     <div className="profile-container">
        <header>
@@ -45,7 +51,7 @@ export default function Profile(){
         <span>Bem, vida {nameOng}</span>
 
         <Link className="button" to="/incidentes/new">Cadastrar novo caso</Link>
-        <button type="button">
+        <button type="button" onClick={handleLougout}>
           <FiPower size={18} color="#E02041"/>
         </button>
        </header>
