0d03f1095b6c94c1a6d92755c273d8cafe8e1b69
leosdesousa12 <leosdesousa@gmail.com>
2020-04-04 21:24:12 -0300

    Move backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const OngController = require('./controller/OngController');
 const IncidentController = require('./controller/IncidentController');
 const ProfileController = require('./controller/ProfileController');
+const LoginController = require('./controller/LoginController');
 
 const routes = express.Router();
 
@@ -13,5 +14,6 @@ routes.post('/incidents', IncidentController.create);
 routes.get('/incidents', IncidentController.list);
 routes.delete('/incidents/:id', IncidentController.delete);
 routes.get('/profile', ProfileController.listId);
+routes.post('/sessions', LoginController.create);
 
 module.exports = routes;
